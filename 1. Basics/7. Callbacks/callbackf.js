94d346acabed8149f2a29c7e6d3d604d2a9b95d1
fjbanezares <fjbanezares@gmail.com>
2023-10-23 03:40:10 +0200

    new modules to the course

--- 1. Basics/7. Callbacks/callbackf.js
+++ 1. Basics/7. Callbacks/callbackf.js
@@ -3,7 +3,7 @@
 
 function processData(data, callback) {
     // Process the data (e.g., sort, filter, transform)
-    const processedData = data.sort();
+    const processedData = data.sort(); //alphabetically
 
     // Call the callback function with the processed data
     callback(processedData);
@@ -15,7 +15,7 @@ function displayData(data) {
 }
 
 // Sample data
-const unprocessedData = [5, 2, 8, 1, 4];
+const unprocessedData = [5, 2, 8, 1, 4, 24];
 
 // Call the processData function and pass the displayData function as a callback
 processData(unprocessedData, displayData);
--- 2. DOM/3. drum game/finish/index.js
+++ 2. DOM/3. drum game/finish/index.js
@@ -1,30 +1,42 @@
+// Initialize the total number of drum buttons present in the document.
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+// Loop through each drum button to assign event listeners.
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  // Add a click event listener to each drum button.
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
+    // Retrieve the content (character) of the clicked button.
     var buttonInnerHTML = this.innerHTML;
 
+    // Trigger the corresponding sound based on the button's character.
     makeSound(buttonInnerHTML);
 
+    // Activate the animation for the button.
     buttonAnimation(buttonInnerHTML);
 
   });
-
 }
 
-document.addEventListener("keypress", function(event) {
+// Add an event listener for keypress events to the entire document.
+document.addEventListener("keypress", function (event) {
 
+  // Trigger the sound corresponding to the key that was pressed.
   makeSound(event.key);
 
+  // Activate the animation for the corresponding button.
   buttonAnimation(event.key);
 
 });
 
-
+/**
+ * Function to play a specific sound based on the provided key.
+ * @param {string} key - The character representing a specific sound.
+ */
 function makeSound(key) {
 
+  // Use a switch statement to determine which sound to play.
   switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
@@ -61,21 +73,26 @@ function makeSound(key) {
       kick.play();
       break;
 
-
-    default: console.log(key);
-
+    // Handle any unexpected input.
+    default:
+      console.log("Key not mapped to any sound:", key);
   }
 }
 
-
+/**
+ * Function to animate a button upon being activated.
+ * @param {string} currentKey - The character representing a specific button.
+ */
 function buttonAnimation(currentKey) {
 
+  // Target the active button using its class.
   var activeButton = document.querySelector("." + currentKey);
 
+  // Add the 'pressed' class to create a visual effect.
   activeButton.classList.add("pressed");
 
-  setTimeout(function() {
+  // Remove the 'pressed' class after a brief delay to reset the button's appearance.
+  setTimeout(function () {
     activeButton.classList.remove("pressed");
   }, 100);
-
 }
